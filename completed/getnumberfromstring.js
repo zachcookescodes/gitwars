376f6ad85cf12719d2daffcc37ed88a6551af6ee
ZachCooke <zachcookescodes@gmail.com>
2022-05-13 06:36:23 -0400

    morning kata

---
+++ completed/getnumberfromstring.js
@@ -0,0 +1,42 @@
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+
+
+
+
+function getNumberFromString(s) {
+  let x = s.split('')
+//   for(i=0;1<s.length;i++){
+//     if (typeof x[i] === NaN){
+      
+//     }
+//   }
+
+
+
+function getNumbersFromStrings(s) = {
+    return +s.split('').filter(a=> a >= 0 && a <= 9).join('')
+}
+
+
+
+
+//   let z = x.filter(y=> y === 'number')
+//   let d = z.join('')
+//   return +d
+// }
+
+
+
+
+// function getNumberFromString(s) {
+//     return Number(s.split('').filter(a => a <= '9' && a >= '0').join(''))
+    
+    
+//   }
+
