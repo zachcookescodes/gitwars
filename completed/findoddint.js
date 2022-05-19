524a05a47a9cbcfff9b30c3a4ed4378bb4b3b0dc
ZachCooke <zachcookescodes@gmail.com>
2022-05-19 07:32:14 -0400

    xoroodint

--- completed/findoddint.js
+++ completed/findoddint.js
@@ -14,7 +14,8 @@
 function findOdd(a) {
     // input = array of integers
     // output = 1 intetger
-    // filter array for array for copies
+    // filter will filter a new array for each elemtent it runs on in this case because it is going to run find until the conidition is satisfied
+    // find ends up being a kind of loop
     // find the single odd copy
     //
       return a.find((x) => a.filter(y => y == x).length % 2 )
@@ -47,3 +48,59 @@ function findOdd(a) {
 
 // const findOdd = (xs) = xs.reduce((a,b) => a ^ b)
 // how?
+
+
+// ^ = means XOR operation
+// Capital letters as A or B or X are the result of a xor operation.
+
+// 1. Truth table
+// --- 
+
+// 0 ^ 0 = 0
+// 0 ^ 1 = 1
+// 1 ^ 0 = 1
+// 1 ^ 1 = 0
+
+// (this is usefull to undertand second thing below)
+
+// 2. Properties: 
+
+// A ^ A = 0 ( ex: 10 ^ 10 = 0 )
+// A ^ 0 = A 
+
+// (ps: we don't need other properties to undertand)
+
+// 3. Associativity:
+
+// a ^ b ^ c = a ^ c ^ b
+// or even
+// a ^ b ^ c ^ d = a ^ c ^ d ^ b
+
+// So this means that the priority order of operations 
+// can be changed, this is not mandatory to start by doing a ^ b operation.
+
+// OK now real examples/practice
+
+// [10, 3, 20, 10, 3, 20, 10]
+// here 10 is the number that is repeated odd times
+
+// Ps: if you don't know how reduce acts, check this:
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/reduce
+
+// This solution will iterate like this.
+// - 10 ^ 3 = A (it's 9 but we dont need to know real results)
+// - A ^ 20 = B it's the same as 10 ^ 3 ^ 20 so B = 10 ^ 3 ^ 20 ..and so on
+// - 10 ^ 3 ^ 20 ^ 10. At this moment we can use associativity, change the order or prio operations
+//   so we can write 10 ^ 10 ^ 3 ^ 20, now use the properties (A ^ A = 0)
+//   so 10 ^ 10 = 0 ... then 0 ^ 3 ^ 20. Again use the property (A ^ 0 = A)..
+//   so 0 ^ 3 ^ 20 = 3 ^ 20. we continu iteration ... 
+  
+// - 3 ^ 20 ^ 3 .. Again use associativity and properties, the result here is 20
+// - 20 ^ 20 = 0, then last iteration
+// - 0 ^ 10 = 10 ! 
+
+// As you see the behaviour is than: if at a time we meet/encounter a number thats already IN previous 
+// XOR operations .. like : [a] ^ b ^ c ^ [a] the reapeated number is somehow canceled or removed.
+
+// Thats why XOR operation can resolve this kind of problem. but only with this particulary prerequisites.
+
