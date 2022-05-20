c8fda33b1494ab10c25f0ee34187b3eda4f4f50b
ZachCooke <zachcookescodes@gmail.com>
2022-05-20 12:45:25 -0400

    goodafternoon

--- completed/mumbling.js
+++ completed/mumbling.js
@@ -7,6 +7,28 @@
 
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-gives me an array of strings
-s.split('').forEach((x,y) => x.repeat(y+1)).join('-')
 
+// p: string
+// r: same string with each letter the index number of times separated by -
+// e: see above
+// p:
+
+
+function accum(s) {
+    // convert entire string to uppercase
+    // split string into array by character
+    // map a new array that takes each elemenet and add the same element lower case repeated index amount of times
+    // rejoin array into string by -
+    return s.toUpperCase().split('').map((x,y) => x += x.toLowerCase().repeat(y)).join('-')
+    
+  }
+  
+  
+
+//   function accum(s) {
+//     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+//   }
+
+// function accum(s) {
+//     return s.split('').map((x,index) => x.toUpperCase()+Array(index+1).join(x.toLowerCase())).join('-');
+//   }
