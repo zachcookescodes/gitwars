943b8853a89e27bdbd1f45f0112615dd051acbe7
ZachCooke <zachcookescodes@gmail.com>
2022-06-07 19:07:15 -0400

    count-sum

---
+++ completed/countpossumneg.js
@@ -0,0 +1,24 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+function countPositivesSumNegatives(input) {
+    if (input === null || input.length === 0)
+        return []
+    
+    let count = 0
+    let sum = 0
+    for (i=0; i < input.length; i++){
+    if (input[i] > 0){
+      sum += 1
+   }else if ( input[i] < 0){
+     count += input[i]}  
+    }
+    return [sum, count]
+  }
