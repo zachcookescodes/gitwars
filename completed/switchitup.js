69754922fff2d8b87d6d401f4998fe0e7467736b
ZachCooke <zachcookescodes@gmail.com>
2022-08-09 17:08:36 -0400

    switchitup

---
+++ completed/switchitup.js
@@ -0,0 +1,30 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+
+
+
+
+
+
+function switchItUp(number){
+    return{
+      0: 'Zero',
+      1: 'One',
+      2: 'Two',
+      3: 'Three',
+      4: 'Four',
+      5: 'Five',
+      6: 'Six',
+      7: 'Seven',
+      8: 'Eight',
+      9: 'Nine'
+    }[number]
+  }
+  
+  // switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
