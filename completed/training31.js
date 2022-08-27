4b3e4b96a57a6276572025354e28d7d611908283
ZachCooke <zachcookescodes@gmail.com>
2022-08-27 14:17:57 -0400

    training31

---
+++ completed/training31.js
@@ -0,0 +1,40 @@
+// Coding in function blackAndWhite. function accept 1 parameter arr(a number array).
+
+// If arr is not an array, function should return:
+
+// "It's a fake array"
+
+// If arr contains elements 5 and 13, function should return:
+
+// "It's a black array"
+
+// If arr contains neither 5 nor 13, function should return:
+
+// "It's a white array"
+
+// Examples
+
+// blackAndWhite(5,13) should return "It's a fake array"
+// blackAndWhite([5,13]) should return "It's a black array"
+// blackAndWhite([5,12]) should return "It's a white array" 
+
+
+function blackAndWhite(arr){
+    if (Array.isArray(arr) === false){
+      return "It's a fake array"
+    }else if(arr.includes(5) && arr.includes(13) === true){
+      return "It's a black array"
+    }else{
+      return "It's a white array"
+    }
+  }
+
+
+// function blackAndWhite(arr){
+//     if ( !Array.isArray( arr ) ) return 'It\'s a fake array'
+//     if ( arr.indexOf( 5 ) != -1 && arr.indexOf( 13 ) != -1 ) return 'It\'s a black array'
+//     return 'It\'s a white array'
+//   }
+
+// const blackAndWhite = arr =>
+//   `It's a ${!Array.isArray(arr) ? `fake` : arr.includes(5) && arr.includes(13) ? `black` : `white`} array`;
