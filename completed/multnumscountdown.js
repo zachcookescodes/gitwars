c52a89b216f3479d87312d5a0823739dc3021c20
ZachCooke <zachcookescodes@gmail.com>
2022-11-01 17:23:40 -0400

    multsnumcountdown

---
+++ completed/multnumscountdown.js
@@ -0,0 +1,13 @@
+// Your task is to write function factorial.
+
+// https://en.wikipedia.org/wiki/Factorial
+
+
+
+function factorial(n){
+    let arr = []
+    for(i=1;i<=n;i++){
+      arr.push(i)
+    }
+    return arr.reduce((a,b) => a*b, 1)
+  }
