ae14b57687b4eb4469e2ae7388928b86bafcc129
ZachCooke <zachcookescodes@gmail.com>
2022-12-24 14:16:55 -0500

    repeatnumber

---
+++ completed/repeatnumber.js
@@ -0,0 +1,25 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+// Examples
+
+// explode("312")
+
+// should return :
+
+// "333122"
+
+// explode("102269")
+
+// should return :
+
+// "12222666666999999999"
+
+
+function explode(s) {
+    // p => string
+    // r => string with each numerical value presented number of times it's numerical value
+    // e => 
+    // p => 
+      
+      return s.split('').map((x)=> x.repeat(x)).join('')
+      
+    }
