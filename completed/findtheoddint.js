aed3c3ffb93fefe0c60557b6b59025ef5d9adbc4
ZachCooke <zachcookescodes@gmail.com>
2023-01-02 19:23:53 -0500

    findtheoddint

---
+++ completed/findtheoddint.js
@@ -0,0 +1,19 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+// Examples
+
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(a) {
+    // input = array of integers
+    // output = 1 intetger
+    // filter array for array with
+    // find the single odd copy
+    //
+      return a.find((x) => a.filter(y => y == x).length % 2 )
+  }
