5d061d2f1895d332635196fcdd5d31d639bd369a
ZachCooke <zachcookescodes@gmail.com>
2023-01-13 18:54:22 -0500

    geomprogressivesequence

---
+++ completed/geomprogseq.js
@@ -0,0 +1,16 @@
+// In your class, you have started lessons about geometric progression. Since you are also a programmer, you have decided to write a function that will print first n elements of the sequence with the given constant r and first element a.
+
+// Result should be separated by comma and space.
+// Example
+
+// geometricSequenceElements(2, 3, 5) == '2, 6, 18, 54, 162'
+
+
+function geometricSequenceElements(a, r, n){
+    let newArr = [a]
+    let prod = a
+    for(i=1; i<n; i++){
+      newArr.push(prod*=r) 
+    }
+    return newArr.join(', ')
+  }
