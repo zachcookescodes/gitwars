2f21baa209743f7e8d892574ca46f9d57233bae6
ZachCooke <zachcookescodes@gmail.com>
2023-02-20 14:57:01 -0500

    destroyduplicates

---
+++ completed/destroyduplicates.js
@@ -0,0 +1,22 @@
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
+
+
+
+function removeDuplicateWords (s) {
+    // your perfect code...
+    let words = s.split(' ')
+    let solos = new Set (words)
+    return Array.from(solos).toString().split(',').join(' ')
+  }
+  
