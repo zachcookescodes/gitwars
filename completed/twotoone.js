c4ba328363c345fb8a0753d27ebc5aca8eac421b
ZachCooke <zachcookescodes@gmail.com>
2023-03-02 14:15:17 -0500

    twotoone

---
+++ completed/twotoone.js
@@ -0,0 +1,20 @@
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+
+
+
+function longest(s1, s2) {
+   let arr = s1.concat(s2).split('').sort()
+   return arr.filter((x,i) => arr.indexOf(x) === i ).join('')
+  
+}
