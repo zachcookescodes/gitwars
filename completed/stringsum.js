e6b9523a33b16c45a9044b8d164253d398201081
ZachCooke <zachcookescodes@gmail.com>
2023-06-25 12:04:22 -0400

    strngsum

---
+++ completed/stringsum.js
@@ -0,0 +1,9 @@
+  // Sum Strings as Numbers
+  // JavaScript:Given the string representations of two integers, return the string representation of the sum of those integers.
+
+function sumStrings(a,b) { 
+  return (BigInt(a) + BigInt(b)).toString();
+}
+
+
+
